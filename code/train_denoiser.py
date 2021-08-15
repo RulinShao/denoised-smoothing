507ee08ac7ba54e984f33568023198c5ecc09807
RulinShao <Srl0310@outlook.com>
2021-08-16 05:39:39 +0800

    debug

--- code/train_denoiser.py
+++ code/train_denoiser.py
@@ -168,8 +168,9 @@ def main():
             test_loss = test(test_loader, denoiser, criterion, args.noise_sd, args.print_freq, args.outdir)
             test_acc = 'NA'
         elif args.objective in ['classification', 'stability']:
-            train_loss = train(train_loader, denoiser, criterion, optimizer, epoch, args.noise_sd, clf)
-            if args.dataset == 'imagenet': 
+            # train_loss = train(train_loader, denoiser, criterion, optimizer, epoch, args.noise_sd, clf)
+            train_loss = 0.0
+            if args.dataset == 'imagenet':
                 test_loss, test_acc = test_with_classifier(test_loader, denoiser, criterion, args.noise_sd, args.print_freq, clf)
             else:
                 # This is needed so that cifar10 denoisers trained using imagenet32 are still evaluated on the cifar10 testset
