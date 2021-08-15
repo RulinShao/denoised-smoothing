c2ecc56824b2a5d229b3e2ba426be903a47d0982
RulinShao <Srl0310@outlook.com>
2021-08-15 22:53:30 +0800

    add vit script

--- code/run_train_denoiser.sh
+++ code/run_train_denoiser.sh
@@ -3,7 +3,7 @@ export IMAGENET_DIR=~/dataset/ilsvrc2012
 python3 train_denoiser.py \
   --dataset imagenet \
   --objective stability \
-  --noise-sd 0.25 \
+  --noise_sd 0.25 \
   --arch imagenet_dncnn \
   --outdir denoiser/vit_small/ \
   --classifier vit_small_patch16_224 \
