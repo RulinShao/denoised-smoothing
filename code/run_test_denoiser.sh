23fd719c2d661ebfe5cea24655a1c373c92dce94
RulinShao <Srl0310@outlook.com>
2021-08-25 13:29:24 +0800

    test resnet18 denoiser

--- code/run_test_denoiser.sh
+++ code/run_test_denoiser.sh
@@ -4,8 +4,8 @@ export IMAGENET_DIR=~/dataset/ilsvrc2012
 python3 test_denoiser.py \
   --dataset imagenet \
   --noise_sd 0.25 \
-  --denoiser denoiser/vit_small/best.pth.tar \
+  --denoiser denoiser/resnet18/best.pth.tar \
   --clf vit_small_patch16_224 \
-  --outdir test_deno/vit_small/ \
+  --outdir test_deno/resnet18/ \
   --batch 128 \
   --gpu 0,1,2,3
