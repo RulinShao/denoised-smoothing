6cc4eb4d5e1fa0da090db1062ce82e7430e1f83e
RulinShao <Srl0310@outlook.com>
2021-08-25 12:20:23 +0800

    add bash for test denoiser

---
+++ code/run_test_denoiser.sh
@@ -0,0 +1,11 @@
+
+export IMAGENET_DIR=~/dataset/ilsvrc2012
+
+python3 test_denoiser.py \
+  --dataset imagenet \
+  --noise_sd 0.25 \
+  --denoiser denoiser/vit_small/best.pth.tar \
+  --clf vit_small_patch16_224 \
+  --outdir test_deno/vit_small/ \
+  --batch 128 \
+  --gpu 0,1,2,3
--- code/run_train_denoiser.sh
+++ code/run_train_denoiser.sh
@@ -1,28 +1,13 @@
-#!bin/bash
-
 export IMAGENET_DIR=~/dataset/ilsvrc2012
 
-if [ $1 = 'train' ]
-then
-  python3 train_denoiser.py \
-    --dataset imagenet \
-    --objective stability \
-    --noise_sd 0.25 \
-    --arch imagenet_dncnn \
-    --outdir denoiser/vit_small/ \
-    --classifier vit_small_patch16_224 \
-    --epochs 25 \
-    --lr 1e-5 \
-    --batch 64
-elif [ $1 = 'test']
-then
-  python3 test_denoiser.py \
-  --dataset imagenet \
-  --noise_sd 0.25 \
-  --denoiser denoiser/vit_small/best.pth.tar \
-  --clf vit_small_patch16_224 \
-  --outdir test_deno/vit_small/ \
-  --batch 128 \
-  --gpu 0,1,2,3
-fi
 
+python3 train_denoiser.py \
+  --dataset imagenet \
+  --objective stability \
+  --noise_sd 0.25 \
+  --arch imagenet_dncnn \
+  --outdir denoiser/vit_small/ \
+  --classifier vit_small_patch16_224 \
+  --epochs 25 \
+  --lr 1e-5 \
+  --batch 64
