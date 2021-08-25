4fbaa949d4ce0b6573b48d773027d4da5da0f325
RulinShao <Srl0310@outlook.com>
2021-08-25 17:18:39 +0800

    certify

--- code/run_certify_denoiser.sh
+++ code/run_certify_denoiser.sh
@@ -4,6 +4,6 @@ python3 certify.py \
   --dataset imagenet \
   --base_classifier resnet18 \
   --sigma 0.25 \
-  --outfile certi_deno/resnet18 \
+  --outfile certi_deno/resnet18/sigma_0.25 \
   --skip 20 \
   --denoiser denoiser/resnet18/best.pth.tar \
