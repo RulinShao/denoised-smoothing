942f2b0d874fab8dfebe4423dcbae4e1b996ae99
RulinShao <Srl0310@outlook.com>
2021-08-27 07:08:29 +0800

    plot certified robustness

---
+++ code/my_plot_certify.py
@@ -0,0 +1,32 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+sns.set()
+
+
+class Accuracy(object):
+    def at_radii(self, radii: np.ndarray):
+        raise NotImplementedError()
+
+
+class ApproximateAccuracy(Accuracy):
+    def __init__(self, data_file_path: str):
+        self.data_file_path = data_file_path
+
+    def at_radii(self, radii: np.ndarray) -> np.ndarray:
+        df = pd.read_csv(self.data_file_path, delimiter="\t")
+        return np.array([self.at_radius(df, radius) for radius in radii])
+
+    def at_radius(self, df: pd.DataFrame, radius: float):
+        return (df["correct"] & (df["radius"] >= radius)).mean()
+
+    def get_abstention_rate(self) -> np.ndarray:
+        df = pd.read_csv(self.data_file_path, delimiter="\t")
+        return 1.*(df["predict"]==-1).sum()/len(df["predict"])*100
+
+
+
+if __name__ == '__main__':
+    file = ApproximateAccuracy('certi_deno/vit_small/sigma_25')
+    abstention_rate = file.get_abstention_rate()
+    # radii = file.at_radii()
