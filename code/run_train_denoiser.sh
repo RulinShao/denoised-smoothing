977f00a6588c1d0f887b101b074d58a845f0fa7b
RulinShao <Srl0310@outlook.com>
2021-08-27 08:24:16 +0800

    train denoiser for lenet

--- code/run_train_denoiser.sh
+++ code/run_train_denoiser.sh
@@ -6,7 +6,7 @@ python3 train_denoiser.py \
   --objective stability \
   --noise_sd 0.25 \
   --arch imagenet_dncnn \
-  --outdir denoiser/lenet/ \
+  --outdir denoiser/lenet/sigma_25 \
   --classifier lenet \
   --epochs 25 \
   --lr 1e-5 \
