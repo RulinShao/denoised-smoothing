277c98c5c2b6f43ded07162fde248156e6c1488e
RulinShao <srl0310@outlook.com>
2022-07-14 17:29:46 +0000

    minor update

--- code/run_train_denoiser.sh
+++ code/run_train_denoiser.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export IMAGENET_DIR=/home/ubuntu/data/ilsvrc2012/
 
-for model in deit_tiny_patch16_224 swin_tiny_patch4_window7_224 vgg16 deit_tiny_distilled_patch16_224
+for model in deit_tiny_patch16_224 swin_tiny_patch4_window7_224 vgg16 deit_tiny_distilled_patch16_224 vit_tiny_patch16_22 deit_small_patch16_224 deit_small_distilled_patch16_224 swin_small_patch4_window7_224
 do
   echo $model
   python train_denoiser.py \
