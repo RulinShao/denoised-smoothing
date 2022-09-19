f615198a5e8ac50caa1396ac7063f1489526cf6d
RulinShao <srl0310@outlook.com>
2022-09-19 19:28:33 +0000

    added clip certify code

--- code/run_certify_denoiser.sh
+++ code/run_certify_denoiser.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 export IMAGENET_DIR=/home/ubuntu/data/ilsvrc2012/
 
-# for model in vgg16 deit_tiny_patch16_224 deit_small_patch16_224 deit_small_distilled_patch16_224
-for model in vgg16 deit_tiny_distilled_patch16_224
-do
-  echo $model
-  python certify.py \
-    --dataset imagenet \
-    --base_classifier $model \
-    --sigma 0.25 \
-    --outfile certi_deno/$model/sigma_25 \
-    --skip 20 \
-    --denoiser denoiser/$model/sigma_25/best.pth.tar
-done
+# # for model in vgg16 deit_tiny_patch16_224 deit_small_patch16_224 deit_small_distilled_patch16_224
+# for model in vgg16 deit_tiny_distilled_patch16_224
+# do
+#   echo $model
+#   python certify.py \
+#     --dataset imagenet \
+#     --base_classifier $model \
+#     --sigma 0.25 \
+#     --outfile certi_deno/$model/sigma_25 \
+#     --skip 20 \
+#     --denoiser denoiser/$model/sigma_25/best.pth.tar
+# done
+
+
+python certify_clip.py \
+  --dataset imagenet \
+  --sigma 0.25 \
+  --outfile certi_deno/clip_vit16/sigma_25 \
+  --skip 20 \
+  --denoiser denoiser/clip_vit16/sigma_25/best.pth.tar
